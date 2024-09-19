3dfd172ecc63537d19e010d36e2759d35c4fb0b7
BAGRATIM <bagratmchedlidze@gmail.com>
2024-09-19 20:02:38 +0400

    Add Python files

---
+++ homerwork_3/task_3_2.py
@@ -0,0 +1,8 @@
+#არამთელი რიცხვის გენერაცია და მეათებამდე დამრგვალებულის დაბეჭდვა
+from random import uniform
+
+random_float = uniform(1, 100000)  
+print(random_float)
+
+print(round(random_float, 1))
+
